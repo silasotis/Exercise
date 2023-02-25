3cca97ce5eebcd66757fafe79fd0cb2eeef9fe46
silasotis <silasogot@gmail.com>
2023-02-25 08:17:49 -0800

    my commit

---
+++ ary.py
@@ -0,0 +1,8 @@
+#initialize data and sum
+data = [1, 2, 7, 9, 5, 17, 13, 15, 7, 20, 4]
+sum = 0
+#sum of the values in data
+for value in data:
+    sum = sum + value
+#display sum
+print("The sum is", sum)
